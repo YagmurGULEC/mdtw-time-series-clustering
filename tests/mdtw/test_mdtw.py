448d5e21d5e9bad5b76e4828724591b75c84f98d
YagmurGULEC <yagmurgulec89@gmail.com>
2025-05-05 08:38:52 -0400

    Unit tests modified

--- tests/mdtw/test_mdtw.py
+++ tests/mdtw/test_mdtw.py
@@ -19,14 +19,14 @@ def test_distance_matrix(prepared_data):
     Test the distance matrix calculation.
     """
     # Calculate the distance matrix
-    distance_matrix = calculate_distance_matrix(prepared_data,traditional=True)
-    assert (distance_matrix[0,0] == 0)
-
-    print (distance_matrix)
-     
+    distance_matrix = calculate_distance_matrix(prepared_data)
+    # Check diagonal is zero
+    np.testing.assert_array_equal(np.diag(distance_matrix), np.zeros(len(prepared_data)))
+    
     # Check the shape of the distance matrix
     assert distance_matrix.shape == (len(prepared_data), len(prepared_data)), "Distance matrix shape is incorrect."
-
-    # Check the values in the distance matrix
-    # assert np.allclose(distance_matrix[0, 0], 0), "Distance from person_1 to themselves should be 0."
-    # assert np.allclose(distance_matrix[1, 1], 0), "Distance from person_2 to themselves should be 0."
+     # Check that all off-diagonal elements are greater than 0
+    for i in range(len(prepared_data)):
+        for j in range(len(prepared_data)):
+            if i != j:
+                assert distance_matrix[i, j] > 0, f"Distance between {i} and {j} should be greater than 0"
